5eadff5d4692dd91940153f5e7e1a04718e1f70d
Aidan Dempster <aidan.dempster@gmail.com>
2018-01-26 19:02:06 -0500

    Changed Join Commands to use extend PIDCommand in order to simplify code
    PID values now tuned within each Join Commands

--- robot/src/main/java/org/iraiders/robot2018/robot/commands/arm/LowerJointCommand.java
+++ robot/src/main/java/org/iraiders/robot2018/robot/commands/arm/LowerJointCommand.java
@@ -1,22 +1,18 @@
 package org.iraiders.robot2018.robot.commands.arm;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 import org.iraiders.robot2018.robot.subsystems.ArmSubsystem;
 
-public class LowerJointCommand extends Command {
+public class LowerJointCommand extends PIDCommand {
   private WPI_TalonSRX motor;
   private Potentiometer motorPot;
   private double setDeg;
   
-  private ArmSubsystem armSubsystem;
-  private PIDController pid;
-  
   public LowerJointCommand(ArmSubsystem armSubsystem, double angle){
+    super(0.025, 0, 0); //TODO: Tune PID
     requires(armSubsystem);
-    this.armSubsystem = armSubsystem;
     this.motor = armSubsystem.getLowerJoint();
     this.motorPot = armSubsystem.getLowerPot();
     this.setDeg = angle;
@@ -24,18 +20,21 @@ public class LowerJointCommand extends Command {
   
   @Override
   protected void initialize() {
-    pid = armSubsystem.createPIDController(1, motorPot, motor); //TODO: Get an actually meaningful tolerance value
-    pid.setSetpoint(setDeg);
-    pid.enable();
+    setSetpoint(setDeg);
   }
   
   @Override
-  protected void end(){
-    pid.disable();
+  protected double returnPIDInput() {
+    return motorPot.get();
+  }
+  
+  @Override
+  protected void usePIDOutput(double output) {
+    motor.set(output);
   }
   
   @Override
   protected boolean isFinished() {
-    return pid.onTarget();
+    return getPIDController().onTarget();
   }
 }
