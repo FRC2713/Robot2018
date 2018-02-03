51f3c4ccc4c4a17e6dc88c3e125cd433e1e1096f
Conlan <conlanc@gmail.com>
2018-02-02 21:09:33 -0500

    Add a max speed for OI drive set in prefs
    Use this as a multiplier, ie .5 will be half speed, -.5 will be reverse and half, etc.
    Set inside the SmartDashboard prefs widget

--- robot/src/main/java/org/iraiders/robot2018/robot/commands/OIDrive.java
+++ robot/src/main/java/org/iraiders/robot2018/robot/commands/OIDrive.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 import org.iraiders.robot2018.robot.OI;
+import org.iraiders.robot2018.robot.Robot;
 import org.iraiders.robot2018.robot.RobotMap;
 import org.iraiders.robot2018.robot.subsystems.DriveSubsystem;
 
@@ -25,19 +26,20 @@ public class OIDrive extends Command {
   
   @Override
   protected void execute() {
+    float maxSpeed = Robot.prefs.getFloat("OIMaxSpeed", 1);
     // Invert directions, on an XBox controller the forward direction is negative
     switch (mode) {
       case BRADFORD:
-        drive.roboDrive.arcadeDrive(-xbox.getY(Hand.kLeft), xbox.getX(Hand.kRight), true);
+        drive.roboDrive.arcadeDrive(-xbox.getY(Hand.kLeft) * maxSpeed, xbox.getX(Hand.kRight), true);
         break;
       
       case ARCADE:
-        drive.roboDrive.arcadeDrive(-xbox.getY(Hand.kLeft), xbox.getX(Hand.kLeft), true);
+        drive.roboDrive.arcadeDrive(-xbox.getY(Hand.kLeft) * maxSpeed, xbox.getX(Hand.kLeft), true);
         break;
       
       default:
       case TANK:
-        drive.roboDrive.tankDrive(-xbox.getY(Hand.kLeft), -xbox.getY(Hand.kRight), true);
+        drive.roboDrive.tankDrive(-xbox.getY(Hand.kLeft) * maxSpeed, -xbox.getY(Hand.kRight) * maxSpeed, true);
         break;
     }
   }
