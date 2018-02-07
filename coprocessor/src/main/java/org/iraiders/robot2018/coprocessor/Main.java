d7504dc0b0b4727f618cdfcc6a31052d7addbe4b
Conlan <conlanc@gmail.com>
2018-02-06 19:01:00 -0500

    Remove java from Coprocessor in prep for python based vision

--- coprocessor/src/main/java/org/iraiders/robot2018/coprocessor/Main.java
+++
@@ -1,7 +0,0 @@
-package org.iraiders.robot2018.coprocessor;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Vision stuff may appear here someday");
-  }
-}
